d501458de64a5277222d8fe1f7bd2f79d9bd5543
jonatanooo <72115156+jonatanooo@users.noreply.github.com>
2026-04-13 20:56:16 -0600

    Add files via upload

---
+++ Fase 1 C#/Fase 1 C#/Login.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fase_1_C_
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Enfermera Abrirenferm = new Enfermera();
+
+            Abrirenferm.Show();
+            this.Hide();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            VistaPacientes abrirlista = new VistaPacientes();
+            abrirlista.Show();
+            this.Hide();
+        }
+    }
+}
